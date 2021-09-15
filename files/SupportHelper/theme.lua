7026536bf2b43082e8728d44b1f86333c041b3cc
Zoom-Developer <73240796+Zoom-Developer@users.noreply.github.com>
2021-09-15 23:04:34 +0400

    Create theme.lua

---
+++ files/SupportHelper/theme.lua
@@ -0,0 +1,66 @@
+local imgui_lib = require 'imgui'
+
+EXPORTS = {
+
+    SwitchColorTheme = function()
+        local style = imgui_lib.GetStyle()
+        local colors = style.Colors
+        local clr = imgui_lib.Col
+        local ImVec4 = imgui_lib.ImVec4
+
+        style.WindowRounding = 2
+        style.WindowTitleAlign = imgui_lib.ImVec2(0.5, 0.5)
+        style.ChildWindowRounding = 4.0
+        style.FrameRounding = 3
+        style.ItemSpacing = imgui_lib.ImVec2(5.0, 4.0)
+        style.ScrollbarSize = 13.0
+        style.ScrollbarRounding = 0
+        style.GrabMinSize = 8.0
+        style.GrabRounding = 1.0
+        style.WindowPadding = imgui_lib.ImVec2(4.0, 4.0)
+        style.FramePadding = imgui_lib.ImVec2(3.5, 3.5)
+        style.ButtonTextAlign = imgui_lib.ImVec2(0.0, 0.5)
+
+        colors[clr.Text] = ImVec4(0.80, 0.80, 0.83, 1.00)
+        colors[clr.TextDisabled] = ImVec4(0.24, 0.23, 0.29, 1.00)
+        colors[clr.WindowBg] = ImVec4(0.06, 0.05, 0.07, 1.00)
+        colors[clr.ChildWindowBg] = ImVec4(0.07, 0.07, 0.09, 1.00)
+        colors[clr.PopupBg] = ImVec4(0.07, 0.07, 0.09, 1.00)
+        colors[clr.Border] = ImVec4(0.80, 0.80, 0.83, 0.88)
+        colors[clr.BorderShadow] = ImVec4(0.92, 0.91, 0.88, 0.00)
+        colors[clr.FrameBg] = ImVec4(0.10, 0.09, 0.12, 1.00)
+        colors[clr.FrameBgHovered] = ImVec4(0.24, 0.23, 0.29, 1.00)
+        colors[clr.FrameBgActive] = ImVec4(0.56, 0.56, 0.58, 1.00)
+        colors[clr.TitleBg] = ImVec4(0.76, 0.31, 0.00, 1.00)
+        colors[clr.TitleBgCollapsed] = ImVec4(1.00, 0.98, 0.95, 0.75)
+        colors[clr.TitleBgActive] = ImVec4(0.80, 0.33, 0.00, 1.00)
+        colors[clr.MenuBarBg] = ImVec4(0.10, 0.09, 0.12, 1.00)
+        colors[clr.ScrollbarBg] = ImVec4(0.10, 0.09, 0.12, 1.00)
+        colors[clr.ScrollbarGrab] = ImVec4(0.80, 0.80, 0.83, 0.31)
+        colors[clr.ScrollbarGrabHovered] = ImVec4(0.56, 0.56, 0.58, 1.00)
+        colors[clr.ScrollbarGrabActive] = ImVec4(0.06, 0.05, 0.07, 1.00)
+        colors[clr.ComboBg] = ImVec4(0.19, 0.18, 0.21, 1.00)
+        colors[clr.CheckMark] = ImVec4(1.00, 0.42, 0.00, 0.53)
+        colors[clr.SliderGrab] = ImVec4(1.00, 0.42, 0.00, 0.53)
+        colors[clr.SliderGrabActive] = ImVec4(1.00, 0.42, 0.00, 1.00)
+        colors[clr.Button] = ImVec4(0.10, 0.09, 0.12, 1.00)
+        colors[clr.ButtonHovered] = ImVec4(0.24, 0.23, 0.29, 1.00)
+        colors[clr.ButtonActive] = ImVec4(0.56, 0.56, 0.58, 1.00)
+        colors[clr.Header] = ImVec4(0.10, 0.09, 0.12, 1.00)
+        colors[clr.HeaderHovered] = ImVec4(0.56, 0.56, 0.58, 1.00)
+        colors[clr.HeaderActive] = ImVec4(0.06, 0.05, 0.07, 1.00)
+        colors[clr.ResizeGrip] = ImVec4(0.00, 0.00, 0.00, 0.00)
+        colors[clr.ResizeGripHovered] = ImVec4(0.56, 0.56, 0.58, 1.00)
+        colors[clr.ResizeGripActive] = ImVec4(0.06, 0.05, 0.07, 1.00)
+        colors[clr.CloseButton] = ImVec4(0.40, 0.39, 0.38, 0.5)
+        colors[clr.CloseButtonHovered] = ImVec4(0.40, 0.39, 0.38, 0.39)
+        colors[clr.CloseButtonActive] = ImVec4(0.40, 0.39, 0.38, 1.00)
+        colors[clr.PlotLines] = ImVec4(0.40, 0.39, 0.38, 0.63)
+        colors[clr.PlotLinesHovered] = ImVec4(0.25, 1.00, 0.00, 1.00)
+        colors[clr.PlotHistogram] = ImVec4(0.40, 0.39, 0.38, 0.63)
+        colors[clr.PlotHistogramHovered] = ImVec4(0.25, 1.00, 0.00, 1.00)
+        colors[clr.TextSelectedBg] = ImVec4(0.25, 1.00, 0.00, 0.43)
+        colors[clr.ModalWindowDarkening] = ImVec4(1.00, 0.98, 0.95, 0.73)
+
+    end
+}

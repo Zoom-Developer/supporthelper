58e2d71e4c01d2214a7a772dacfc5e750a4145e8
Zoom-Developer <73240796+Zoom-Developer@users.noreply.github.com>
2021-09-17 14:33:04 +0400

    Add files via upload

---
+++ files/lib/broadcaster.lua
@@ -0,0 +1,126 @@
+-- This file is part of broadcaster library
+-- Licensed under MIT License
+-- Copyright (c) 2019 Ranx
+-- https://github.com/r4nx/broadcaster
+-- Version 0.2.0
+
+local handlers = {}
+
+local proto = require 'broadcaster.proto'
+local utils = require 'broadcaster.utils'
+local encoder = require 'broadcaster.encoder'
+local charset = require 'broadcaster.charset'
+local magic = require 'broadcaster.magic'
+
+local inspect = require 'inspect'
+
+-- Args:
+--    handlerId <string> - unique handler id
+--    callback <function> - callback function
+--    rawData <bool> [optional] - do not decode data before passing to callback
+function EXPORTS.registerHandler(handlerId, callback, rawData)
+    -- Doing some check in advance to avoid undefined behavior
+    if type(handlerId) ~= 'string' or not encoder.check(handlerId, charset.MESSAGE_ENCODE) then
+        error(('invalid handler id ("%s")'):format(handlerId))
+    end
+    if handlers[handlerId] ~= nil then
+        error(('handler id collision: handler "%s" has been already registered'):format(handlerId))
+    end    
+    if type(callback) ~= 'function' then
+        error(('callback object is not a function (handler id "%s")'):format(handlerId))
+    end
+
+    handlers[handlerId] = {callback, rawData}
+end
+
+-- Args:
+--    handlerId <string> - handler id to unregister
+-- Returns:
+--    bool - true if unregistered successfully
+function EXPORTS.unregisterHandler(handlerId)
+    if handlers[handlerId] ~= nil then
+        handlers[handlerId] = nil
+        return true
+    end
+    return false
+end
+
+local function bitsToBitStream(bits)
+    local bs = raknetNewBitStream()
+    for _, bValue in ipairs(bits) do
+        raknetBitStreamWriteBool(bs, bValue == 1 and true or false)
+    end
+    return bs
+end
+
+-- Args:
+--    message <string> - message to send
+--    handlerId <string> - remote handler id
+function EXPORTS.sendMessage(message, handlerId)
+    if type(message) ~= 'string' or not encoder.check(message, charset.MESSAGE_ENCODE) then
+        error(('invalid message ("%s")'):format(message))
+    end
+    if type(handlerId) ~= 'string' or not encoder.check(handlerId, charset.MESSAGE_ENCODE) then
+        error(('invalid handler id ("%s")'):format(handlerId))
+    end
+
+    local encodedMessage = encoder.encode(message, charset.MESSAGE_ENCODE)
+    local encodedHandlerId = encoder.encode(handlerId, charset.MESSAGE_ENCODE)
+
+    local packets = proto.sendData(encodedMessage, encodedHandlerId)
+    for _, p in ipairs(packets) do
+        local bs = bitsToBitStream(p)
+        raknetBitStreamSetWriteOffset(bs, 16)
+        raknetSendRpc(magic.RPC_OUT, bs)
+        raknetDeleteBitStream(bs)
+    end
+end
+
+function EXPORTS._printHandlers()
+    print('Handlers:')
+    for handlerId, handlerData in pairs(handlers) do
+        print(handlerId, inspect(handlerData))
+    end
+end
+
+function EXPORTS._printSessions()
+    print('Sessions:\n' .. inspect(proto.getSessions()))
+end
+
+local function bitStreamToBits(bs)
+    local bits = {}
+    for _ = 1, raknetBitStreamGetNumberOfUnreadBits(bs) do
+        bits[#bits + 1] = raknetBitStreamReadBool(bs) and 1 or 0
+    end
+    return bits
+end
+
+local function sessionHandler(session)
+    local handlerId = encoder.decode(session.handlerId, charset.MESSAGE_DECODE)
+    local handler, rawData = unpack(handlers[handlerId] or {})
+    if handler ~= nil then
+        if rawData then
+            handler(session.data)
+        else
+            handler(encoder.decode(session.data, charset.MESSAGE_DECODE))
+        end
+    end
+end
+
+local function rpcHandler(rpcId, bs)
+    if rpcId == magic.RPC_IN and utils.tableLength(handlers) > 0 then
+        raknetBitStreamResetReadPointer(bs)
+
+        local bits = bitStreamToBits(bs)
+
+        if #bits == magic.PACKETS_LEN then
+            proto.processPacket(bits, sessionHandler)
+        else
+        end
+    end
+end
+
+function main()
+    addEventHandler('onReceiveRpc', rpcHandler)
+    wait(-1)
+end
---
+++ files/lib/broadcaster/proto.lua
@@ -0,0 +1,118 @@
+-- This file is part of broadcaster library
+-- Licensed under MIT License
+-- Copyright (c) 2019 Ranx
+-- https://github.com/r4nx/broadcaster
+-- Version 0.2.0
+
+local proto = {}
+
+local packet = require 'broadcaster.packet'
+local utils = require 'broadcaster.utils'
+local Session = require 'broadcaster.session'
+local inspect = require 'inspect'
+
+local sessions = {}
+
+-- Args:
+--    bin <table> - binary sequence
+--    callback <function> - function to call when session is finished
+--      (session is passed as first argument to callback)
+function proto.processPacket(bin, callback)
+    proto.collectOldSessions()
+    local packetCode = utils.binToDec({unpack(bin, 1, 3)})
+
+    packetProcessors = utils.switch({
+        [packet.PACKETS_ID.START_TRANSFER] = function()
+            local startPacket = packet.StartTransferPacket.unpack(bin)
+            local sessionId = startPacket.sessionId
+
+            if sessions[sessionId] ~= nil then
+                sessions[sessionId] = nil
+                return
+            end
+
+            sessions[sessionId] = Session()
+        end,
+        [packet.PACKETS_ID.DATA] = function()
+            local result, returned = pcall(packet.DataPacket.unpack, bin)
+            if not result then
+                return
+            end
+            local sessionId = returned.sessionId
+
+            local sess = sessions[sessionId]
+            if sess ~= nil then
+                sess:appendData(returned.data)
+            else
+            end
+        end,
+        [packet.PACKETS_ID.HANDLER_ID] = function()
+            local result, returned = pcall(packet.HandlerIdPacket.unpack, bin)
+            if not result then
+                return
+            end
+            local sessionId = returned.sessionId
+
+            local sess = sessions[sessionId]
+            if sess ~= nil then
+                sess:appendHandlerId(returned.handlerId)
+            else
+            end
+        end,
+        [packet.PACKETS_ID.STOP_TRANSFER] = function()
+            local stopTransferPacket = packet.StopTransferPacket.unpack(bin)
+            local sessionId = stopTransferPacket.sessionId
+
+            local sess = sessions[sessionId]
+            if sess ~= nil then
+                sessions[sessionId] = nil
+                callback(sess)
+            else
+            end
+        end,
+        default = function() end
+    })
+
+    packetProcessors:case(packetCode)
+end
+
+local function randomSessionId()
+    math.randomseed(os.time() ^ 5)
+    return math.random(0, 15)  -- 4 bit
+end
+
+-- Args:
+--    data <table> - table of decimal numbers
+--    handlerId <table> - encoded handler id
+-- Returns:
+--    Table of binary sequences
+function proto.sendData(data, handlerId)
+    local sessionId = randomSessionId()
+    local packets = {packet.StartTransferPacket(sessionId):pack()}
+
+    for _, handlerIdPart in ipairs(handlerId) do
+        packets[#packets + 1] = packet.HandlerIdPacket(handlerIdPart, sessionId):pack()
+    end
+
+    for _, dataPart in ipairs(data) do
+        packets[#packets + 1] = packet.DataPacket(dataPart, sessionId):pack()
+    end
+
+    packets[#packets + 1] = packet.StopTransferPacket(sessionId):pack()
+
+    return packets
+end
+
+function proto.collectOldSessions()
+    for sessionId, sess in pairs(sessions) do
+        if os.time() - sess.lastUpdate > 30 then
+            sessions[sessionId] = nil
+        end
+    end
+end
+
+function proto.getSessions()
+    return sessions
+end
+
+return proto
